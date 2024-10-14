d61b6a7ac0ba669daf415ef18458ee9608702643
linxi <1595818766@qq.com>
2024-10-14 09:59:10 +0800

    请求测试

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -1,12 +1,43 @@
-import React, { memo } from "react";
+import lxRequest from "@/service";
+import React, { memo, useEffect, useState } from "react";
 import type { FC, ReactNode } from "react";
 
 interface IProps {
   children?: ReactNode; // 或者React.ReactNode
 }
+
+export interface IBannerData {
+  imageUrl: string;
+  targetId: number;
+  targetType: number;
+  titleColor: string;
+  typeTitle: string;
+  exclusive: boolean;
+  encodeId: string;
+  scm: string;
+  bannerBizType: string;
+}
+
 // 或者React.FC
 const Recommend: FC<IProps> = () => {
-  return <div>Recommend</div>;
+  const [banners, setBanners] = useState<IBannerData[]>([]);
+
+  useEffect(() => {
+    lxRequest
+      .get({
+        url: "/banner",
+      })
+      .then((res) => {
+        setBanners(res.banners);
+      });
+  }, []);
+  return (
+    <div>
+      {banners.map((item, index) => {
+        return <div key={index}>{item.imageUrl}</div>;
+      })}
+    </div>
+  );
 };
 
 export default memo(Recommend);
