a053c4de39372353f2072e63c065e9acaacd9287
linxi <1595818766@qq.com>
2024-10-14 14:39:35 +0800

    开发环境生产环境配置变量

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+// 同名的interface再次定义会对原来的进行一次扩展
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly REACT_APP_BASE_URL: string;
+  }
+}
+ 
--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,2 +1,15 @@
 export const BASE_URL = process.env.BASE_URL || "http://codercba.com:9002";
 export const TIME_OUT = Number(process.env.TIME_OUT) || 10000;
+
+
+/* // 方法二根据环境导出
+let BASE_URL = "";
+// process.env.NODE_ENV是webpack提供的
+if (process.env.NODE_ENV === "development") {
+  BASE_URL = "http://codercba.dev:9002";
+} else {
+  BASE_URL = "http://codercba.prod:9002";
+}
+ */
+// 方法三配置.env文件
+// console.log(process.env);
