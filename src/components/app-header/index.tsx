be391854a7faa6977e80042a8eff64f211ce81ec
linxi <1595818766@qq.com>
2024-10-21 09:20:02 +0800

    使用styled-components进行样式编写,使用theme混入

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from "react";
 import type { FC, ReactNode } from "react";
 import { Link } from "react-router-dom";
+import { HeaderWrapper } from "./styled";
 
 interface IProps {
   children?: ReactNode; // 或者React.ReactNode
@@ -9,15 +10,17 @@ interface IProps {
 const AppHeader: FC<IProps> = () => {
   return (
     <div>
-      <div className='nav'>
-        <Link to='/discover'>发现音乐</Link>
-        <Link to='/mine'>我的音乐</Link>
-        <Link to='/focus'>关注</Link>
-        <Link to='/download'>下载客户端</Link>
-        <Link to='/template'>模板使用</Link>
-      </div>
+      <HeaderWrapper>
+        <div className="content">
+          <Link to='/discover'>发现音乐</Link>
+          <Link to='/mine'>我的音乐</Link>
+          <Link to='/focus'>关注</Link>
+          <Link to='/download'>下载客户端</Link>
+          <Link to='/template'>模板使用</Link>
+        </div>
+      </HeaderWrapper>
     </div>
-  )
+  );
 };
 
 export default memo(AppHeader);
