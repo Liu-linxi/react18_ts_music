a682ff980b1f0a8b954f04345dab15e63679b6e9
linxi <1595818766@qq.com>
2024-10-21 11:00:51 +0800

    选择切换路由标记标识active使用NavLink

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,6 +1,6 @@
 import React, { memo } from "react";
 import type { FC, ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { HeaderLeft, HeaderRight, HeaderWrapper } from "./styled";
 import type { HeaderLinksType } from "@/type/local-data";
 import { headerLinks } from "@/service/local-data";
@@ -9,42 +9,43 @@ interface IProps {
 }
 // 或者React.FC
 const AppHeader: FC<IProps> = () => {
-  const showItem = (item:HeaderLinksType) => {
+  const showItem = (item: HeaderLinksType) => {
     if (item.type === "path") {
       return (
-        <Link to={item.link}>
+        <NavLink to={item.link}>
           {item.title}
-          <i className="sprite_01 icon"></i>
-        </Link>
-      )
+          <i className='sprite_01 icon'></i>
+        </NavLink>
+      );
     } else {
-      return <a href={item.link} target="_blank" rel="noopener noreferrer">{item.title}</a>
+      return (
+        <a href={item.link} target='_blank' rel='noopener noreferrer'>
+          {item.title}
+        </a>
+      );
     }
-  }
+  };
   return (
-    <div>
-      <HeaderWrapper>
-        <div className='content'>
-          <HeaderLeft>
-            <a className='logo sprite_01' href='#/'>
-              网易云音乐
-            </a>
-            <div className="select-list">
-            {
-              headerLinks.map((item) => {
-                return (
-                  <div className="select-item" key={item.title}>
-                    {showItem(item)}
-                  </div>
-                )
-              })
-            }
+    <HeaderWrapper>
+      <div className='content'>
+        <HeaderLeft>
+          <a className='logo sprite_01' href='#/'>
+            网易云音乐
+          </a>
+          <div className='select-list'>
+            {headerLinks.map((item) => {
+              return (
+                <div className='select-item' key={item.title}>
+                  {showItem(item)}
+                </div>
+              );
+            })}
           </div>
-          </HeaderLeft>
-          <HeaderRight></HeaderRight>
-        </div>
-      </HeaderWrapper>
-    </div>
+        </HeaderLeft>
+        <HeaderRight></HeaderRight>
+      </div>
+      <div className='divider'></div>
+    </HeaderWrapper>
   );
 };
 
