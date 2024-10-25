ead5c945b13e0b7e83717a5bb5bc467b7cde43bf
linxi <1595818766@qq.com>
2024-10-25 17:33:04 +0800

    新增

--- src/views/player/app-play-bar/index.tsx
+++ src/views/player/app-play-bar/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useRef, useState } from "react";
 import type { FC, ReactNode } from "react";
 import { Control, Operator, PlaybarWrapper, PlayInfo } from "./style";
 import { NavLink } from "react-router-dom";
@@ -14,21 +14,30 @@ const AppPlayerBar: FC<IProps> = () => {
   const { currentSong } = useAppSelector((state) => ({
     currentSong: state.player.currentSong,
   }));
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const audioRef=useRef<HTMLAudioElement>(null)
+
+  const handlePlayBtnClick = useCallback(() => {
+    setIsPlaying(!isPlaying);
+    isPlaying ? audioRef.current?.pause() : audioRef.current?.play().catch(err => {
+      setIsPlaying(false);
+    });
+  }, [isPlaying]);
 
   return (
     <PlaybarWrapper className='sprite_playbar'>
       <div className='content wrap-v2'>
-        <Control>
+        <Control isPlaying={isPlaying}>
           <button className='sprite_playbar btn prev'></button>
-          <button className='sprite_playbar btn play'></button>
+          <button className='sprite_playbar btn play' onClick={() => handlePlayBtnClick()}></button>
           <button className='sprite_playbar btn next'></button>
         </Control>
         <PlayInfo>
           <div className='image'>
             <NavLink to='/discover/player'>
               {/* <img src='https://p2.music.126.net/OVkXDNmbk2uj6wE1KTZIwQ==/109951165203334337.jpg?param=34y34' alt='' /> */}
-              <img src={getSizeImage(currentSong?.al?.picUrl,34)} alt='' />
-
+              <img src={getSizeImage(currentSong?.al?.picUrl, 34)} alt='' />
             </NavLink>
           </div>
           <div className='info'>
@@ -37,7 +46,7 @@ const AppPlayerBar: FC<IProps> = () => {
               <span className='singer-name'>{currentSong.ar[0].name}</span>
             </div>
             <div className='progress'>
-              <Slider value={23} />
+              <Slider value={progress} />
               <div className='time'>
                 <span className='now-time'>{formatMinuteSecond(123 * 1000)}</span>
                 <span className='divider'>/</span>
@@ -59,6 +68,7 @@ const AppPlayerBar: FC<IProps> = () => {
           </div>
         </Operator>
       </div>
+      <audio ref={audioRef}/>
     </PlaybarWrapper>
   );
 };
